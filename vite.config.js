8fd6960977b07e81218bd3e7e2dd8c127c3c5e51
Ddeiv <davidebocchi?gmail.com>
2026-02-16 16:57:33 +0100

    stable version 1.0

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
     base: '/portfolio/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                lavori: resolve(__dirname, 'lavori.html'),
+            },
+        },
+    },
 })
