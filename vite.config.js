f3dffca61a2a3d5daf740751254cf3b02d7611da
Ddeiv <davidebocchi?gmail.com>
2026-02-17 14:33:05 +0100

    Fix base path for custom domain - serve from root

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    base: '/portfolio_test/',
+    base: '/',
     build: {
         rollupOptions: {
             input: {
